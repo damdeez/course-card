bdd42023d5bda1ef9bcfc89a4a7b3ea45f3a89b0
damdeez <damir.hara@gmail.com>
2017-03-26 13:13:31 -0600

    added in functionality when clicking on the arrows, refactored some code, restyled the svg icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../styles/main.scss';
-import {MdAddBox, MdArrowDropDown, MdCreate, MdNoteAdd, MdAnnouncement, MdModeComment, MdFolder} from 'react-icons/lib/md';
+import {MdAddBox, MdArrowDropDown, MdArrowDropUp, MdCreate, MdNoteAdd, MdAnnouncement, MdModeComment, MdFolder} from 'react-icons/lib/md';
 
 export default class Card extends React.Component {
     constructor(props) {
@@ -16,23 +16,24 @@ export default class Card extends React.Component {
                 classMarkup.push(
                     <li key={index} className="card">
                         <header className="class-heading">
-                            <span><MdAddBox /></span>
+                            <span className="svg"><MdAddBox /></span>
                             <img src={`${path}uofu-${index}.jpg`} alt="Class Image"/>
                         </header>
                         <div className="inner-card">
-                            <h2>{data.name}</h2>
-                            <h3>{data.code}</h3>
-                            <p className="card-description" onClick={this.expandCardDescription}>
+                            <h2 className="class-title">{data.name}</h2>
+                            <h3 className="class-number">{data.code}</h3>
+                            <div className="card-description" onClick={(e) => this.expandCardDescription(e, index)}>
                                 {data.description}
                                 <span className="description-overlay" />
-                            </p>
-                            <span><MdArrowDropDown /></span>
+                            </div>
+                            <span className="arrow-down svg" onClick={(e) => this.expandCardDescription(e, index)}><MdArrowDropDown /></span>
+                            <span className="arrow-up svg" onClick={(e) => this.expandCardDescription(e, index)}><MdArrowDropUp /></span>
                             <nav className="card-nav">
                                 <ul>
-                                    <li><MdNoteAdd /></li>
-                                    <li><MdAnnouncement /></li>
-                                    <li><MdModeComment /></li>
-                                    <li><MdFolder /></li>
+                                    <li className="svg"><MdNoteAdd /></li>
+                                    <li className="svg"><MdAnnouncement /></li>
+                                    <li className="svg"><MdModeComment /></li>
+                                    <li className="svg"><MdFolder /></li>
                                 </ul>
                             </nav>
                         </div>
@@ -45,14 +46,35 @@ export default class Card extends React.Component {
             <div className="loading">...Loading</div>
         }
     }
-    expandCardDescription(e) {
+    expandCardDescription(e, index) {
+        const $downArrow = document.querySelectorAll('.arrow-down')[index];
+        const $upArrow = document.querySelectorAll('.arrow-up')[index];
+        const $overlay = document.querySelectorAll('.description-overlay')[index];
+
         if (e.target.classList.contains('card-description')) {
             if (e.target.classList.contains('expanded')) {
                 e.target.classList.remove('expanded');
-                e.target.children[0].style.visibility = 'visible';
+                $overlay.style.display = 'block';
+                $downArrow.style.display = 'inline';
+                $upArrow.style.display = 'none';
             } else {
                 e.target.classList.add('expanded');
-                e.target.children[0].style.visibility = 'hidden';
+                $overlay.style.display = 'none';
+                $downArrow.style.display = 'none';
+                $upArrow.style.display = 'inline';
+            }
+        } else if (e.target.parentElement.nodeName === 'SPAN' || e.target.nodeName === 'path') {
+            const $element = document.querySelectorAll('.card-description')[index];
+            if ($element.classList.contains('expanded')) {
+                $element.classList.remove('expanded');
+                $downArrow.style.display = 'inline';
+                $upArrow.style.display = 'none';
+                $overlay.style.display = 'block';
+            } else {
+                $element.classList.add('expanded');
+                $downArrow.style.display = 'none';
+                $upArrow.style.display = 'inline';
+                $overlay.style.display = 'none';
             }
         }
     }
